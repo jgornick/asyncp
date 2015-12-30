b6faf52d66e9e210bfb55b04fc26eb236da84666
Joe Gornick <joe@joegornick.com>
2015-12-30 08:52:58 -0600

    Bump to 0.6.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 
 gulp.task('default', function () {
-  return gulp.src('src/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('dist'));
+    return gulp.src('src/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist'));
 });
