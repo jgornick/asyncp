9c11353094ea4d7eece247f39285bc44997ac4ef
Joe Gornick <joe@joegornick.com>
2016-08-05 14:34:51 -0500

    Add non-zero limit check to async.eachLimit.

--- src/eachLimit.js
+++ src/eachLimit.js
@@ -1,5 +1,9 @@
 import throat from 'throat';
 
 export default function eachLimit(collection, limit, iteratee) {
+    if (! limit > 0) {
+        return Promise.reject(new Error('Limit must be a number greater than 0.'));
+    }
+
     return Promise.all(collection.map(throat(limit, iteratee)));
 };
