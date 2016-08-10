f46f835e37042f3d50b4f0df66006c50cbf24788
Joe Gornick <joe@joegornick.com>
2016-08-09 22:17:58 -0500

    Add limit check of 0 to detectLimit

--- src/detectLimit.js
+++ src/detectLimit.js
@@ -3,6 +3,10 @@ import tryFn from './tryFn';
 import PromiseBreak from './promiseBreak';
 
 export default function detectLimit(collection, limit, predicate, notFound = undefined) {
+    if (! limit > 0) {
+        return Promise.reject(new Error('Limit must be a number greater than 0.'));
+    }
+    
     return Promise.all(collection.map(throat(limit, (item, index, collection) => {
         return tryFn(predicate, item, index, collection)
             .then((result) => {
