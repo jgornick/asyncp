8f462299064baf36f80b1db9fe6f2c103972a6a7
Joe Gornick <joe@joegornick.com>
2016-08-12 17:02:41 -0500

    Update filterLimit with limit check and update to work with external collection modifications

--- src/filterLimit.js
+++ src/filterLimit.js
@@ -1,6 +1,16 @@
-import eachLimit from './eachLimit';
+import throat from 'throat';
+import tryFn from './tryFn';
+
+const ASYNCP_UNDEFINED = '__ASYNCP_UNDEFINED__';
 
 export default function filterLimit(collection, limit, predicate) {
-    return eachLimit(collection, limit, predicate)
-        .then((results) => collection.filter((item, index) => results[index]));
+    if (! limit > 0) {
+        return Promise.reject(new Error('Limit must be a number greater than 0.'));
+    }
+
+    return Promise.all(collection.map(throat(limit, (item, index, collection) =>
+        tryFn(predicate, item, index, collection)
+            .then(result => result === true ? item : ASYNCP_UNDEFINED)
+    )))
+        .then(results => results.filter(item => item != ASYNCP_UNDEFINED));
 };
