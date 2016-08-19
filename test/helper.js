6aa73e7e0dcd6b3ffc4b6d391e771fe231cc269d
Joe Gornick <joe@joegornick.com>
2016-08-18 23:18:38 -0500

    Add accumulator iteratee test helpers

--- test/helper.js
+++ test/helper.js
@@ -37,6 +37,41 @@ export function iterateeDelayWithOrder(order, resolver = id) {
     };
 };
 
+export function accIterateeDelay(resolver = id) {
+    return (acc, value, key, collection) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(
+                () => {
+                    try {
+                        resolve(resolver(acc, value, key, collection));
+                    } catch (e) {
+                        reject(e);
+                    }
+                },
+                value * 25
+            );
+        });
+    };
+};
+
+export function accIterateeDelayWithOrder(order, resolver = id) {
+    return (acc, value, key, collection) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(
+                () => {
+                    order.push(value);
+                    try {
+                        resolve(resolver(acc, value, key, collection));
+                    } catch (e) {
+                        reject(e);
+                    }
+                },
+                value * 25
+            );
+        });
+    };
+};
+
 export function iterateePromise(resolver = id) {
     return (value, key, collection) => {
         return new Promise((resolve, reject) => {
@@ -62,6 +97,31 @@ export function iterateePromiseWithOrder(order, resolver = id) {
     };
 };
 
+export function accIterateePromise(resolver = id) {
+    return (acc, value, key, collection) => {
+        return new Promise((resolve, reject) => {
+            try {
+                resolve(resolver(acc, value, key, collection));
+            } catch (e) {
+                reject(e);
+            }
+        });
+    };
+};
+
+export function accIterateePromiseWithOrder(order, resolver = id) {
+    return (acc, value, key, collection) => {
+        return new Promise(resolve => {
+            order.push(value);
+            try {
+                resolve(resolver(acc, value, key, collection));
+            } catch (e) {
+                reject(e);
+            }
+        });
+    };
+};
+
 export function iterateeNative(resolver = id) {
     return (value, key, collection) => {
         return resolver(value, key, collection);
@@ -72,5 +132,18 @@ export function iterateeNativeWithOrder(order, resolver = id) {
     return (value, key, collection) => {
         order.push(value);
         return resolver(value, key, collection);
+    };
+};
+
+export function accIterateeNative(resolver = id) {
+    return (acc, value, key, collection) => {
+        return resolver(acc, value, key, collection);
+    };
+};
+
+export function accIterateeNativeWithOrder(order, resolver = id) {
+    return (acc, value, key, collection) => {
+        order.push(value);
+        return resolver(acc, value, key, collection);
     };
 };
