98e4995c1f30095852e023a7ca215d1b642a427a
Joe Gornick <joe@joegornick.com>
2016-08-18 23:17:32 -0500

    Wrap reduce and reduceRight iteratee with tryFn

--- src/reduce.js
+++ src/reduce.js
@@ -1,7 +1,9 @@
+import tryFn from './tryFn';
+
 export default function reduce(collection, result, iteratee) {
     return collection.reduce(
         (promise, item, index, collection) => {
-            return promise.then((result) => iteratee(result, item, index, collection));
+            return promise.then((result) => tryFn(iteratee, result, item, index, collection));
         },
         Promise.resolve(result)
     );
--- src/reduceRight.js
+++ src/reduceRight.js
@@ -1,7 +1,9 @@
+import tryFn from './tryFn';
+
 export default function reduceRight(collection, result, iteratee) {
     return collection.reduceRight(
         (promise, item, index, collection) => {
-            return promise.then((result) => iteratee(result, item, index, collection));
+            return promise.then((result) => tryFn(iteratee, result, item, index, collection));
         },
         Promise.resolve(result)
     );
