b8593cdd58cc839b8f2f180bd4342827d1cfdd19
Joe Gornick <joe@joegornick.com>
2016-08-19 09:58:27 -0500

    Fix someSeries method signature

--- src/someSeries.js
+++ src/someSeries.js
@@ -2,7 +2,7 @@ import detectSeries from './detectSeries';
 
 const NOT_FOUND = '__ASYNCP_NOT_FOUND__'
 
-export default function someSeries(collection, limit, predicate) {
-    return detectSeries(collection, limit, predicate, NOT_FOUND)
+export default function someSeries(collection, predicate) {
+    return detectSeries(collection, predicate, NOT_FOUND)
         .then((result) => result !== NOT_FOUND);
 };
