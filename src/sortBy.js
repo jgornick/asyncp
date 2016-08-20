42933f12ba87f5f86306b08e5d13d13b8028cc59
Joe Gornick <joe@joegornick.com>
2016-08-20 14:55:16 -0500

    Clean sortBy with custom comparator

--- src/sortBy.js
+++ src/sortBy.js
@@ -1,13 +1,13 @@
 import tryFn from './tryFn';
 
-export default function sortBy(collection, iteratee, sorter) {
-    if (sorter == null) {
-        sorter = (a, b) => a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0;
+export default function sortBy(collection, iteratee, comparator) {
+    if (comparator == null) {
+        comparator = (a, b) => a < b ? -1 : a > b ? 1 : 0;
     }
 
-    return Promise.all(collection.map((item, index, collection) => {
-        return tryFn(iteratee, item, index, collection)
-            .then((result) => [result, item]);
-    }))
-        .then((collection) => collection.sort(sorter).map((item) => item[1]));
+    return Promise.all(collection.map((item, index, collection) =>
+        tryFn(iteratee, item, index, collection)
+            .then((result) => [result, item])
+    ))
+        .then((results) => results.sort(([a], [b]) => comparator(a, b)).map(([sort, item]) => item));
 };
