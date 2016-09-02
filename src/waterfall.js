873c88e4107c22d865b8327203650fc42e6a65db
Joe Gornick <joe@joegornick.com>
2016-09-01 22:33:52 -0500

    Update waterfall to return the last task results unchanged.

--- src/waterfall.js
+++ src/waterfall.js
@@ -2,16 +2,24 @@ import tryFn from './tryFn';
 import WaterfallError from './waterfallError';
 
 export default function waterfall(tasks, ...args) {
+    if (!Array.isArray(tasks)) {
+        return Promise.reject(new Error('First argument to waterfall must be an array of functions'))
+    }
+
     let
         results = [];
 
     return tasks.reduce(
-        (promise, task) => promise.then((promiseResults) => {
+        (promise, task, index) => promise.then((promiseResults) => {
             return tryFn(task, ...promiseResults)
                 .then((taskResults) => {
-                    return results = Array.isArray(taskResults)
-                        ? taskResults
-                        : [taskResults];
+                    if (index == tasks.length - 1) {
+                        return taskResults;
+                    } else {
+                        return results = Array.isArray(taskResults)
+                            ? taskResults
+                            : [taskResults];
+                    }
                 });
         }),
         Promise.resolve(args)
