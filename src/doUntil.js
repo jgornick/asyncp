37ac7e536a3041d1f5b534d59dbae4626e9b6b25
Joe Gornick <joe@joegornick.com>
2016-09-04 14:10:29 -0500

    Update doUntil to pass initial args and task result args to each condition/task.

--- src/doUntil.js
+++ src/doUntil.js
@@ -2,5 +2,10 @@ import tryFn from './tryFn';
 import until from './until';
 
 export default function doUntil(task, condition, ...args) {
-    return tryFn(task, ...args).then(() => until(condition, task));
+    return tryFn(task, ...args)
+        .then((result) =>
+            Array.isArray(result)
+                ? until(condition, task, ...result)
+                : until(condition, task, result)
+        );
 };
