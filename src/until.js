8c1a334c8903d132879d152aea94d1240e6fa631
Joe Gornick <joe@joegornick.com>
2016-09-04 14:09:16 -0500

    Update until to pass initial args and task result args to each condition/task.

--- src/until.js
+++ src/until.js
@@ -1,10 +1,14 @@
 import tryFn from './tryFn';
 
 export default function until(condition, task, ...args) {
-    return tryFn(condition)
+    return tryFn(condition, ...args)
         .then((conditionResult) => {
             return conditionResult
                 ? Promise.resolve()
-                : tryFn(task, ...args).then(() => until(condition, task, ...args));
+                : tryFn(task, ...args).then((result) =>
+                    Array.isArray(result)
+                        ? until(condition, task, ...result)
+                        : until(condition, task, result)
+                );
         });
 };
