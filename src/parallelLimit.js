e9162cffdf651de8809ce77615c44f6bc803bf78
Joe Gornick <joe@joegornick.com>
2016-09-05 14:05:44 -0500

    Add limit check to parallelLimit

--- src/parallelLimit.js
+++ src/parallelLimit.js
@@ -2,5 +2,9 @@ import throat from 'throat';
 import tryFn from './tryFn';
 
 export default function parallelLimit(tasks, limit, ...args) {
+    if (! limit > 0) {
+        return Promise.reject(new Error('Limit must be a number greater than 0.'));
+    }
+
     return Promise.all(tasks.map(throat(limit, (task) => tryFn(task, ...args))));
 };
