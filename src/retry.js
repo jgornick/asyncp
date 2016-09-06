caf11ac828cfcf62372180eb6d5e044862900224
Joe Gornick <joe@joegornick.com>
2016-09-06 10:23:27 -0500

    Update retry to better support different variations of allowed arguments

--- src/retry.js
+++ src/retry.js
@@ -1,14 +1,43 @@
 import tryFn from './tryFn';
 import PromiseBreak from './promiseBreak';
 
-export default function retry({ times = 5, interval = 0 }, task, ...args) {
-    if (typeof interval != 'function') {
-        interval = (function(interval) {
-            return function() { return interval; };
-        })(interval);
+export default function retry(opts, ...args) {
+    let task;
+
+    if (typeof opts == 'function') {
+        task = opts;
+        opts = {
+            times: 5,
+            interval: 0
+        };
     }
 
-    return new Array(times).fill(null)
+    if (Number.isInteger(opts)) {
+        opts = {
+            times: opts,
+            interval: 0
+        };
+    }
+
+    if (!Number.isInteger(opts.times)) {
+        opts.times = 5;
+    }
+
+    if (opts.interval == null) {
+        opts.interval = 0;
+    }
+
+    if (typeof opts.interval != 'function') {
+        opts.interval = (function(interval) {
+            return function() { return interval; };
+        })(opts.interval);
+    }
+
+    if (task == null) {
+        task = args.shift();
+    }
+
+    return new Array(opts.times).fill(null)
         .reduce(
             (promise, item, index) => {
                 return promise.then((results) => {
@@ -23,9 +52,9 @@ export default function retry({ times = 5, interval = 0 }, task, ...args) {
 
                             results.push(error);
 
-                            if (index < (times - 1)) {
+                            if (index < (opts.times - 1)) {
                                 return new Promise(
-                                    (resolve) => setTimeout(() => resolve(results), interval(index + 1))
+                                    (resolve) => setTimeout(() => resolve(results), opts.interval(index + 1, results))
                                 );
                             }
 
