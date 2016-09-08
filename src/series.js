24118e3421538778061943bbfc624aa49a6816cb
Joe Gornick <joe@joegornick.com>
2016-09-08 16:04:19 -0500

    Add tasks array check in series

--- src/series.js
+++ src/series.js
@@ -1,6 +1,10 @@
 import tryFn from './tryFn'
 
 export default function series(tasks, ...args) {
+    if (!Array.isArray(tasks)) {
+        return Promise.reject(new Error('First argument to series must be an array of functions'))
+    }
+
     return tasks.reduce(
         (promise, task) => promise.then((results) => {
             return tryFn(task, ...args)
