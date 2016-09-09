454c40da37c77ecf101b2e4e64e8d22e26b59cb2
Joe Gornick <joe@joegornick.com>
2016-09-09 14:53:35 -0500

    Initial commit of liftFn test

---
+++ test/liftFn.js
@@ -0,0 +1,16 @@
+import async from '../src/async';
+
+describe('liftFn', function() {
+    it('returns a promise', function() {
+        const f = async.liftFn((foo) => {
+            expect(foo).to.equal(0);
+            return foo;
+        });
+        expect(f).to.be.instanceof(Function);
+        const p = f(0);
+        expect(p).to.be.instanceof(Promise);
+        return Promise.all([
+            p.should.eventually.deep.equal(0)
+        ]);
+    });
+});
