5bc1a9c802d74c7a5bfa31c5543f87b5d6df7c94
Joe Gornick <joe@joegornick.com>
2016-09-09 14:45:31 -0500

    Fix WaterfallError to properly extend Error

--- src/waterfallError.js
+++ src/waterfallError.js
@@ -1,20 +1,8 @@
-export default class WaterfallError extends Error {
-    get results() {
-        return this._results;
-    }
-
-    set results(results) {
-        if (this._results == null) {
-            this._results = results;
-        }
-
-        return this;
-    }
-
-    constructor(...args) {
-        super(...args);
-
-        this.name = 'WaterfallError';
-        this._results = null;
-    }
+module.exports = function WaterfallError(message, results) {
+    Error.captureStackTrace(this, this.constructor);
+    this.name = this.constructor.name;
+    this.message = message;
+    this.results = results;
 };
+
+require('util').inherits(module.exports, Error);
