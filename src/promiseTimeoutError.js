ec93aac352ea495ae7d37289d57df179a28319e8
Joe Gornick <joe@joegornick.com>
2016-09-09 14:45:08 -0500

    Fix PromiseTimeoutError to properly extend Error

--- src/promiseTimeoutError.js
+++ src/promiseTimeoutError.js
@@ -1,7 +1,7 @@
-export default class PromiseTimeoutError extends Error {
-    constructor(...args) {
-        super(...args);
-
-        this.name = 'PromiseTimeoutError';
-    }
+module.exports = function PromiseTimeoutError(message) {
+    Error.captureStackTrace(this, this.constructor);
+    this.name = this.constructor.name;
+    this.message = message;
 };
+
+require('util').inherits(module.exports, Error);
