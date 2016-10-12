fef1b046488742446db7765db5e4f86f919a80ee
Joe Gornick <joe@joegornick.com>
2016-10-12 13:58:21 -0500

    Update filter, filterLimit and filterSeries to support promised arguments

--- src/filterLimit.js
+++ src/filterLimit.js
@@ -1,9 +1,10 @@
 import throat from 'throat';
 import tryFn from './tryFn';
+import promised from './promised';
 
 const ASYNCP_UNDEFINED = '__ASYNCP_UNDEFINED__';
 
-export default function filterLimit(collection, limit, predicate) {
+export default promised(function filterLimit(collection, limit, predicate) {
     if (! limit > 0) {
         return Promise.reject(new Error('Limit must be a number greater than 0.'));
     }
@@ -13,4 +14,4 @@ export default function filterLimit(collection, limit, predicate) {
             .then(result => result === true ? item : ASYNCP_UNDEFINED)
     )))
         .then(results => results.filter(item => item != ASYNCP_UNDEFINED));
-};
+});
--- test/filterSeries.js
+++ test/filterSeries.js
@@ -130,6 +130,24 @@ describe('filterSeries', function() {
         });
     });
 
+    it('supports promised arguments', function() {
+        let order = [];
+        const arr = [3, 2, 1];
+        const p = async.filterSeries(
+            new Promise(resolve => setTimeout(resolve.bind(null, arr), 25)),
+            iterateeDelayWithOrder(order, (x) => x == 2)
+        );
+
+        return Promise.all([
+            p.should.eventually.deep.equal([2]),
+            p.then(() => arr.should.deep.equal([3, 2, 1])),
+            new Promise(resolve => setTimeout(
+                () => resolve(order.should.deep.equal(arr)),
+                8 * 25
+            ))
+        ]);
+    });
+
     it('supports not found', function() {
         const p = async.filterSeries([1, 2, 3], () => false);
 
@@ -170,10 +188,10 @@ describe('filterSeries', function() {
         arr.shift();
 
         return Promise.all([
-            p.should.eventually.deep.equal([2, 1]),
+            p.should.eventually.deep.equal([2]),
             p.then(() => arr.should.deep.equal([3, 2])),
             new Promise(resolve => setTimeout(
-                () => resolve(order.should.deep.equal([4, 3, 2, 1])),
+                () => resolve(order.should.deep.equal([3, 2])),
                 11 * 25
             ))
         ]);
